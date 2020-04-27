d5c94f00188535376b8f6c5edbf287be672bc15b
Ricardo-1998 <00004817@uca.edu.sv>
2020-04-27 16:16:46 -0600

    tarea3 terminada

---
+++ tarea3capas/src/main/java/com/uca/capas/tarea3capas/Tarea3capasApplication.java
@@ -0,0 +1,13 @@
+package com.uca.capas.tarea3capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea3capasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea3capasApplication.class, args);
+	}
+
+}
---
+++ tarea3capas/src/test/java/com/uca/capas/tarea3capas/Tarea3capasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.capas.tarea3capas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea3capasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
